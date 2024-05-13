9bbd5d459f05206a577877769e3ee39c6a23f7e6
Johnson <xj1@umbc.edu>
2024-05-13 16:50:42 -0400

    TEst

---
+++ TGUT.py
@@ -0,0 +1,30 @@
+def create_character():
+    """
+    This function will create the character by getting the name, charisma points, and stealth points.
+    :return: the name, charisma points, and stealth points in a dictionary
+    """
+
+def load_events(event_file_name):
+    """
+    This function will take the file name and return it to a dictionary of events that we can use.
+    :param event_file_name: the name of the file with the events
+    :return: the events in a dictionary that we can use
+    """
+
+def load_map(map_file_name):
+    """
+    This function will take a file with a list of locations and return a map that we can use
+    :param map_file_name: the file with the locations
+    :return: a map for rest of the program to use
+    """
+
+def play_game(start_time, game_map, events):
+    """
+    This function will play our game and use the map and events we got from the other functions to run and keep track of the time remaining
+    :param start_time: the time we have to get to ITE
+    :param game_map: the map that we will use to navigate through UMBC
+    :param events: the dictionary of events that we will use if we reach a location with a event
+    :return: nothing
+    """
+
+if __name__ =="__main__":
